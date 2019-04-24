4615845f09cf3d29bac87d030fe638b258b807ff
Cédric Dupont <lunarok@gmail.com>
2019-04-24 09:57:11 +0200

    Update newsapi.php

--- desktop/php/newsapi.php
+++ desktop/php/newsapi.php
@@ -50,6 +50,7 @@ $eqLogics = eqLogic::byType('newsapi');
     <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a>
     <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
     <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}</a>
+	   <a class="btn btn-default eqLogicAction pull-right" data-action="copy"><i class="fas fa-files-o"></i> {{Dupliquer}}</a>
     <ul class="nav nav-tabs" role="tablist">
       <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fas fa-arrow-circle-left"></i></a></li>
       <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer"></i> {{Equipement}}</a></li>
